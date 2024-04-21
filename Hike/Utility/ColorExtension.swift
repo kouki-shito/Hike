685f8b93209895c7a56c6972cc2ff332c999b2c1
kouki-shito <likegameskou@gmail.com>
2024-04-21 20:53:14 +0900

    2nd quest finished

---
+++ Hike/Utility/ColorExtension.swift
@@ -0,0 +1,21 @@
+//
+//  ColorExtension.swift
+//  Hike
+//
+//  Created by 市東 on 2024/04/21.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+  static let customGreenLight = Color("ColorGreenLight")
+  static let customGreenMedium = Color("ColorGreenMedium")
+  static let customGreenDark = Color("ColorGreenDark")
+  static let customGrayLight = Color("ColorGrayLight")
+  static let customGrayMedium = Color("ColorGrayMedium")
+  static let customIndigoMedium = Color("ColorIndigoMedium")
+  static let customSalmonLight = Color("ColorSalmonLight")
+
+}
+
